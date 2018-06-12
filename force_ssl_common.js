3ec8f09ee0793fd2d4922964d61de36e46a2b132
vu-bui <vubui@qasymphony.com>
2018-06-12 17:37:51 +0700

    fix absoluteUrl always return https

--- force_ssl_common.js
+++ force_ssl_common.js
@@ -1,1 +1,2 @@
-_.extend(Meteor.absoluteUrl.defaultOptions, {secure: true});
+// don't hard-code secure here try to read from FORCE_SSL or something else
+// _.extend(Meteor.absoluteUrl.defaultOptions, {secure: true});
